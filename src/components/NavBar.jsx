c5f13f1e8ddeb4954b71ef621a73f85c3250486c
atharv018 <atharv2415@gmail.com>
2025-08-27 23:40:57 +0530

    Prepare for deploy

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ function NavBar() {
         <nav className="bg-gray-900 h-11 flex text-white px-6 py-2 
                         justify-between align-center shadow-md text-xl font-bold">
             <div>
-                <Link to="/home">MOVIELIST</Link>
+                <Link to="/">MOVIELIST</Link>
             </div>
             <div className="flex flex-row gap-7">
                 <NavLink to="/watchlist" className={({isActive}) => isActive ? "hover:text-red-600" : " hover:text-red-600"}>WatchList</NavLink>
